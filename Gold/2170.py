a4a9003052a0598806f820426b444c7baa253d14
JooJaeHwan <usjjh@naver.com>
2022-12-13 23:48:25 +0900

    2022.12.13

--- Gold/2170.py
+++ Gold/2170.py
@@ -9,3 +9,22 @@ import sys
 
 input = sys.stdin.readline
 
+N = int(input())
+array = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    array.append((x, y))
+array.sort()
+
+start, end = array[0]
+result = 0
+for x, y in array:
+    if x <= end:
+        end = max(end, y)
+    else:
+        result += end - start
+        start = x
+        end = y
+result += end - start
+print(result)
