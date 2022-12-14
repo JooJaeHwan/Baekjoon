8ebaafd35dd1839d9c989637d7c3e2a6348014e3
JooJaeHwan <usjjh@naver.com>
2022-12-14 22:59:31 +0900

    2022.12.14

---
+++ Silver/3273.py
@@ -0,0 +1,16 @@
+'''
+두 수의 합
+https://www.acmicpc.net/problem/3273
+정렬, 투 포인터
+'''
+
+
+import sys
+
+
+input = sys.stdin.readline
+
+N = int(input())
+array = list(map(int, input().split()))
+x = int(input())
+
