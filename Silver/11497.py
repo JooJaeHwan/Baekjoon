1bbe64b388a7ba60540cd441714def2816cbdda8
JooJaeHwan <usjjh@naver.com>
2022-12-15 23:22:14 +0900

    2022.12.15

---
+++ Silver/11497.py
@@ -0,0 +1,12 @@
+'''
+통나무 건너뛰기
+https://www.acmicpc.net/problem/11497
+그리디 알고리즘, 정렬
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
--- Silver/3273.py
+++ Silver/3273.py
@@ -14,3 +14,20 @@ N = int(input())
 array = list(map(int, input().split()))
 x = int(input())
 
+array.sort()
+
+result = 0
+start = 0
+end = N - 1
+
+while start < end:
+    current = array[start] + array[end]
+    if current == x:
+        result += 1
+        start += 1
+    elif current < x:
+        start += 1
+    elif current > x:
+        end -= 1
+    
+print(result)
