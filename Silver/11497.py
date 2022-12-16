1ff2fa1446804fcc87646c7b54c2e534a56c9349
JooJaeHwan <usjjh@naver.com>
2022-12-16 23:56:31 +0900

    2022.12.16

--- Silver/11497.py
+++ Silver/11497.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 
 T = int(input())
 
+for _ in range(T):
+    N = int(input())
+    data = list(map(int, input().split()))
+    data.sort(reverse=True)
+    
+    result = [0] * N
+    result[N // 2] = data[0]
+    for i in range(1, N):
+        if i % 2 == 1:
+            result[N // 2 - i // 2 - 1] = data[i]
+        else:
+            result[N // 2 + i // 2] = data[i]
+
+    max_dif = 0
+    for i in range(N):
+        dif = abs(result[i] - result[(i + 1) % N])
+        max_dif = max(max_dif, dif)
+    print(max_dif)
